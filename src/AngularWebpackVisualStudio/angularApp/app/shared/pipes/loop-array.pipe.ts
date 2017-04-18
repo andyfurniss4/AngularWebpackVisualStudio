279208de6bb1091699a06ad9db90cd878ec1096f
Andrew Furniss <afurniss@globebmg.com>
2017-04-18 16:48:02 +0100

    Add loop array pipe

---
+++ src/AngularWebpackVisualStudio/angularApp/app/shared/pipes/loop-array.pipe.ts
@@ -0,0 +1,12 @@
+﻿import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'loopArray' })
+export class LoopArrayPipe implements PipeTransform {
+    transform(value: number): number[] {
+        let result: number[] = [];
+        for (let i = 0; i < value; i++) {
+            result.push(i);
+        }
+        return result;
+    }
+}

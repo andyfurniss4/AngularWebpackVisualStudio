c29188fcfd50e6f150beced5f89272bec643da97
Andrew Furniss <afurniss@globebmg.com>
2017-04-18 15:32:28 +0100

    Add HttpAuthorisationConfig class

---
+++ src/AngularWebpackVisualStudio/Configuration/HttpAuthorisationConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularWebpackVisualStudio.Configuration
+{
+    public class HttpAuthorisationConfig
+    {
+        public string ApiUrl { get; set; }
+        public string LoginUrl { get; set; }
+        public string LoginResource { get; set; }
+        public string ClientId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string AuthenticationToken { get; set; }
+        public AuthenticationType AuthenticationType { get; set; }
+    }
+
+    public enum AuthenticationType
+    {
+        None = 0,
+        Login,
+        AuthorisationToken
+    }
+}

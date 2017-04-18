7fc5a9c71488c07961c926f6875e11755baaa77d
Andrew Furniss <afurniss@globebmg.com>
2017-04-18 15:12:23 +0100

    Update routes

--- src/AngularWebpackVisualStudio/angularApp/app/app.routes.ts
+++ src/AngularWebpackVisualStudio/angularApp/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    {
-        path: 'about', loadChildren: './about/about.module#AboutModule',
-    }
+    { path: '', redirectTo: 'dynamics-search', pathMatch: 'full' },
+    { path: 'dynamics-search', loadChildren: './dynamics-search/dynamics-search.module#DynamicsSearchModule' }
 ];
 
 export const AppRoutes = RouterModule.forRoot(routes);

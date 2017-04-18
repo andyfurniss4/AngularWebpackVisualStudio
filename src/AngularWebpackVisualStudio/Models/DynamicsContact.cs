5a81bc33a93701d1ecc0b84f47844f7e6a76224c
Andrew Furniss <afurniss@globebmg.com>
2017-04-18 15:44:37 +0100

    Add DynamicsContact and DynamicsSearch models

---
+++ src/AngularWebpackVisualStudio/Models/DynamicsContact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularWebpackVisualStudio.Models
+{
+    public class DynamicsContact
+    {
+        [JsonProperty(PropertyName = "contactid")]
+        public Guid Id;
+        [JsonProperty(PropertyName = "fullname")]
+        public string FullName;
+        [JsonProperty(PropertyName = "gbp_legacycontactid")]
+        public string LegacyContactId;
+    }
+}
---
+++ src/AngularWebpackVisualStudio/Models/DynamicsSearch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularWebpackVisualStudio.Models
+{
+    public class DynamicsSearch
+    {
+        public string entity;
+        public string id;
+        public string filter;
+        public string fields;
+    }
+}

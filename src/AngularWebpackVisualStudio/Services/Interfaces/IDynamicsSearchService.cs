7ede39a35b84943c4f0254b7137d0c970800e7d5
Andrew Furniss <afurniss@globebmg.com>
2017-04-18 15:44:53 +0100

    Add DynamicsSearchService

---
+++ src/AngularWebpackVisualStudio/Services/Interfaces/IDynamicsSearchService.cs
@@ -0,0 +1,13 @@
+﻿using AngularWebpackVisualStudio.Models;
+using System.Collections.Generic;
+
+namespace AngularWebpackVisualStudio.Services
+{
+    public interface IDynamicsSearchService
+    {
+        string GetJsonById(DynamicsSearch searchParams);
+        TEntity GetById<TEntity>(DynamicsSearch searchParams);
+        List<TEntity> Search<TEntity>(DynamicsSearch searchParams);
+        string Search(DynamicsSearch searchParams);
+    }
+}
